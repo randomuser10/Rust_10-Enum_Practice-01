7d7e8af56dd8ef3956997cfa4bfbb79d70e07039
Mayur Satardekar <mayursatardekar10@gmail.com>
2025-08-17 17:35:38 +0530

    Enums with methods

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 
 #[derive(Debug)]
 enum IpAddress {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
+}
+#[derive(Debug)]
+enum Message{
+    Quit,
+    Move(i32, i32),
+    Write(String),
+    ChangeColor(i32,i32,i32),
 }
 
-
+impl Message {
+    fn call(&self){
+                    //
+        
+    }
+}
 
 fn main() {
-    let four = IpAddress::V4;
-    let six = IpAddress::V6;
+    let four = IpAddress::V4(String::from("Hello World"));
+    let six = IpAddress::V6(String::from("How are you?"));
 
     println!("The value of IpAddress4 is {:?}.",four);
     println!("The value of IPAddress6 is {:?}.", six);
+    let m = Message::Write(String::from("Hello"));
+    println!("{:?}", m);
+
+
 }
